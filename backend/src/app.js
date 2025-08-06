b5ad01bfaaa12bf596eff20b26caea3ef219af43
Himanshu Soni <himanshuso@iitbhilai.ac.in>
2025-08-06 23:28:40 +0530

    first commit12

--- backend/src/app.js
+++ backend/src/app.js
@@ -20,10 +20,9 @@ app.use(express.json({ limit: "40kb" }));
 app.use(express.urlencoded({ limit: "40kb", extended: true }));
 
 app.use("/api/v1/users", userRoutes);
-
 const start = async () => {
     app.set("mongo_user")
-    const connectionDb = await mongoose.connect("mongodb+srv://himanshu12695:QsZ7SBZQtmtDX0Nk@letsmeet.ydwlwa1.mongodb.net/")
+    const connectionDb = await mongoose.connect("mongodb+srv://himanshu12695:tJzEdQcZU7bEEvj4@cluster0.mjnxuqa.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 
     console.log(`MONGO Connected DB HOst: ${connectionDb.connection.host}`)
     server.listen(app.get("port"), () => {
